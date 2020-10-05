06f6373e832ea1da7a15bd79a7f1c4f3afd2fba0
bab21 <2016061@iiitdmj.ac.in>
2020-10-05 09:49:02 +0530

    taking user input

--- src/com/capgemini/TicTacToeGame/GameMain.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.TicTacToeGame;
-
-public class GameMain {
-	public static void main(String[] args) {
-		TicTacToeGame tictactoegame=new TicTacToeGame();
-		System.out.println("Starting the tictactoegame");
-		tictactoegame.initializeGame();
-	}
-}
--- src/com/capgemini/TicTacToeGame/TicTacToeGame.java
+++ src/com/capgemini/TicTacToeGame/TicTacToeGame.java
@@ -1,8 +1,37 @@
 package com.capgemini.TicTacToeGame;
+import java.util.*;
+
 
 public class TicTacToeGame {	
-	char[][] board;
-	public void initializeGame() {
+	private char[][] board;
+	TicTacToeGame(){
 		board=new char[4][4];
+	}
+	private void initializeGame() {
+		Arrays.fill(board,' ');
+	}
+	private char[] takeInput() {
+		Scanner s=new Scanner(System.in);
+		char user_choice,computer_choice;
+		char[] choice=new char[2];
+		
+		System.out.println("Pease enter your input(X/O)");
+		user_choice=s.next().charAt(0);
+		computer_choice=' ';
+		
+		if(user_choice=='X')
+			computer_choice='O';
+		else if(user_choice=='O')
+			computer_choice='X';
+		
+		choice[0]=user_choice;
+		choice[1]=computer_choice;
+		
+		return choice;	
+	}
+	public static void main(String[] args) {
+		TicTacToeGame game=new TicTacToeGame();
+		game.initializeGame();
+		char[] choice=game.takeInput();
 	}
 }
