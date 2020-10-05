44ffa0bb9d814dbd2d4e862e1b76901cffb53757
bab21 <2016061@iiitdmj.ac.in>
2020-10-05 09:09:29 +0530

    Starting the tic tac toe game

--- src/com/capgemini/TicTacToeGame/GameMain.java
+++ src/com/capgemini/TicTacToeGame/GameMain.java
@@ -3,6 +3,7 @@ package com.capgemini.TicTacToeGame;
 public class GameMain {
 	public static void main(String[] args) {
 		TicTacToeGame tictactoegame=new TicTacToeGame();
+		System.out.println("Starting the tictactoegame");
 		tictactoegame.initializeGame();
 	}
 }
